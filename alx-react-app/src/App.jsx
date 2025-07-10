5fe19df30baa7c4bab5babece7e1fd1525524deb
Folashade Bello <folashadebello.b@gmail.com>
2025-07-10 08:00:32 +0100

    task 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage.jsx'
 import Header from './components/Header.jsx'
 import MainContent from './components/Maincontent.jsx'
 import Footer from './components/Footer.jsx'
+import UserProfile from './components/UserProfile.jsx'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -36,8 +37,13 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
     </>
-  )
+  );
 }
 
 export default App
