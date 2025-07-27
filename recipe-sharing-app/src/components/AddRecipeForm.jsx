dbe1f12339292062373198324d7d47a039226318
Folashade Bello <folashadebello.b@gmail.com>
2025-07-27 20:05:02 +0100

    corrected

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useRecipeStore } from "../recipestore";
+import { useRecipeStore } from "./recipestore";
 
 const AddRecipeForm = () => {
   const addRecipe = useRecipeStore((state) => state.addRecipe);
