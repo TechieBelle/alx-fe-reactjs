bce1edee04befbe34c2eb455e4fc03ca99702fea
Folashade Bello <folashadebello.b@gmail.com>
2025-07-27 20:09:24 +0100

    corrected

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,26 +1,26 @@
-import { useRecipeStore } from "./recipeStore";
+// import { useRecipeStore } from "./recipeStore";
 
-const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+// const RecipeList = () => {
+//   const recipes = useRecipeStore((state) => state.recipes);
 
-  return (
-    <div>
-      <h2>Recipe List</h2>
-      {recipes.length === 0 ? (
-        <p>No recipes added yet.</p>
-      ) : (
-        recipes.map((recipe) => (
-          <div
-            key={recipe.id}
-            style={{ border: "1px solid #ccc", padding: "10px", margin: "5px" }}
-          >
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h2>Recipe List</h2>
+//       {recipes.length === 0 ? (
+//         <p>No recipes added yet.</p>
+//       ) : (
+//         recipes.map((recipe) => (
+//           <div
+//             key={recipe.id}
+//             style={{ border: "1px solid #ccc", padding: "10px", margin: "5px" }}
+//           >
+//             <h3>{recipe.title}</h3>
+//             <p>{recipe.description}</p>
+//           </div>
+//         ))
+//       )}
+//     </div>
+//   );
+// };
 
-export default RecipeList;
+// export default RecipeList;
