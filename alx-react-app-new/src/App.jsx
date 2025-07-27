2d444fea3dc5cd8097ec1308e9c79377257bbca8
Folashade Bello <folashadebello.b@gmail.com>
2025-07-27 17:25:43 +0100

    task 1

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header.jsx";
 import MainContent from "./components/Maincontent.jsx";
 import Footer from "./components/Footer.jsx";
 import UserProfile from "./components/UserProfile.jsx";
+import Counter from "./components/Counter.jsx";
 
 function App() {
   const bio = "Software engineer with a passion for travel and technology.";
@@ -14,7 +15,8 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <UserProfile name="Belle" age="28" bio = {bio} />
+        <UserProfile name="Belle" age="28" bio={bio} />
+        <Counter />
     </>
   );
 }
---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <p>Current Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
