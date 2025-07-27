3dffb9e4a6676b77940501fe0bec7b734f64c4f2
Folashade Bello <folashadebello.b@gmail.com>
2025-07-27 18:30:00 +0100

    corrected

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import UserContext from "./UserContext";
+import UserContext from "../UserContext";
 
 function UserDetails() {
   // Consume the UserContext using useContext hook
